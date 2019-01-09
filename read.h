f16629a9ea446383abae90d98bf78d8f4280a572
Loïc Dessaules <loic.dessaules@gmail.com>
2019-01-09 10:52:57 +0100

    Filled all headers with functions we'll use

--- read.h
+++ read.h
@@ -9,7 +9,7 @@
 #include <vector>
 
 std::vector<std::string> readFile(const std::string& fileName);
-void inverse(std::vector<std::string>& dict);
-void sortAsc(std::vector<std::string>& dict);
+void inverse(const std::vector<std::string>& dict);
+void sortAsc(const std::vector<std::string>& dict);
 
 #endif //LABO09_READ_H
