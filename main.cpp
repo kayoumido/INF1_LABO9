81266d406f30fd4a2f84abb5f0ebe6af653344d7
Doran Kayoumi <dorankayoumi@gmail.com>
2019-01-10 10:57:08 +0100

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -8,21 +8,22 @@
 using namespace std;
 
 // TODO : I'm a debug function dude ! Don't forgot to remove me later (=
-void displayDict(const vector<string>& dict) {
-  for(auto it = dict.begin(); it != dict.end(); ++it) {
-    cout << *it << endl;
-  }
+void displayDict(const vector<string> &dict) {
+    for (auto it = dict.begin(); it != dict.end(); ++it) {
+        cout << *it << endl;
+    }
 }
 
 int main() {
 
-  vector<string> dict = readFile("nato");
+    vector<string> dict = readFile("nato");
 
-  displayDict(dict);
-  cout << endl;
-  inverse(dict);
-  displayDict(dict);
+    //displayDict(dict);
+    //cout << endl;
+    inverse(dict);
+    sortAsc(dict);
+    displayDict(dict);
 
 
-  return 0;
+    return 0;
 }
--- search.h
+++ search.h
@@ -9,12 +9,25 @@
 #include <vector>
 #include <string>
 
-std::size_t linearSearch(const std::vector<std::string>& dict, const std::string& word);
-std::vector<std::string>::iterator linearSearch(const std::vector<std::string>::iterator& begin, const std::vector<std::string>::iterator& end, const std::string& word);
-std::size_t dichotomySearch(const std::vector<std::string>& dict, const std::string& word);
-std::vector<std::string>::iterator dichotomySearch(const std::vector<std::string>::iterator& begin, const std::vector<std::string>::iterator& end, const std::string& word);
-bool recursiveDichotomySearch(const std::vector<std::string>::iterator& begin, const std::vector<std::string>::iterator& end, const std::string& word);
-bool rechercheDichotomiqueRecursive(const std::vector<std::string>& dict, const std::string& word, std::size_t first = 0, std::size_t last = 10); // TODO trouver les vrais valeurs first et last
+std::size_t linearSearch(const std::vector<std::string> &dict, const std::string &word);
+
+std::vector<std::string>::iterator
+linearSearch(const std::vector<std::string>::iterator &begin, const std::vector<std::string>::iterator &end,
+             const std::string &word);
+
+std::size_t dichotomySearch(const std::vector<std::string> &dict, const std::string &word);
+
+std::vector<std::string>::iterator
+dichotomySearch(const std::vector<std::string>::iterator &begin, const std::vector<std::string>::iterator &end,
+                const std::string &word);
+
+bool
+recursiveDichotomySearch(const std::vector<std::string>::iterator &begin, const std::vector<std::string>::iterator &end,
+                         const std::string &word);
+
+bool
+rechercheDichotomiqueRecursive(const std::vector<std::string> &dict, const std::string &word, std::size_t first = 0,
+                               std::size_t last = 10); // TODO trouver les vrais valeurs first et last
 
 
 #endif //LABO09_SEARCH_H
