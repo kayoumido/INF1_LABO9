2a8da10bdc07e181e8626b9d5cdac88f061c0f85
Doran Kayoumi <dorankayoumi@gmail.com>
2019-01-10 10:56:52 +0100

    Implement bubble sort

--- read.cpp
+++ read.cpp
@@ -8,31 +8,47 @@
 
 using namespace std;
 
-std::vector<std::string> readFile(const std::string &fileName) {
-  string relativePath = DICT_FILE_LOCATION + OS_TYPE + fileName + FILE_EXTENSION;
-  vector<string> content;
-  ifstream ifs;
+vector<string> readFile(const string &fileName) {
+    string relativePath = DICT_FILE_LOCATION + OS_TYPE + fileName + FILE_EXTENSION;
+    vector<string> content;
+    ifstream ifs;
 
-  ifs.open(relativePath);
-  // check if the file was opened
-  if (!ifs.good()) return content;
+    ifs.open(relativePath);
+    // check if the file was opened
+    if (!ifs.good()) return content;
 
-  string line;
-  // read the file line by line
-  while (getline(ifs, line)) {
-    if (line.length() > 0) content.push_back(line);
-  }
-  ifs.close();
+    string line;
+    // read the file line by line
+    while (getline(ifs, line)) {
+        if (line.length() > 0) content.push_back(line);
+    }
+    ifs.close();
 
-  return content;
+    return content;
 }
 
 
-void inverse(vector<string>& dict) {
-    for(size_t i = 0; i < dict.size() / 2; ++i) {
+void inverse(vector<string> &dict) {
+    for (size_t i = 0; i < dict.size() / 2; ++i) {
         string tmp = dict.at(i);
         // Last - 1 to be in range (size() - 0 = out of range and size() - 0 - 1 = in range)
         dict.at(i) = dict.at(dict.size() - i - 1);
         dict.at(dict.size() - i - 1) = tmp;
+    }
+}
+
+void sortAsc(vector<string> &dict) {
+    bool unsorted = true;
+
+    while (unsorted) {
+        unsorted = false;
+        for (size_t i = 0; i < dict.size(); ++i) {
+            if (i + 1 < dict.size() and dict.at(i) > dict.at(i + 1)) {
+                string current = dict.at(i);
+                dict.at(i) = dict.at(i + 1);
+                dict.at(i + 1) = current;
+                unsorted = true;
+            }
+        }
     }
 }
--- read.h
+++ read.h
@@ -13,12 +13,19 @@ const std::string DICT_FILE_LOCATION = "../dico/";
 const std::string OS_TYPE = "unix/";
 const std::string FILE_EXTENSION = ".txt";
 
-std::vector<std::string> readFile(const std::string& fileName);
+/**
+ * TODO
+ * @param fileName
+ * @return
+ */
+std::vector<std::string> readFile(const std::string &fileName);
+
 /**
  * @brief Inverse order of all words in a dictionnary.
  * @param dict The dictionnary as a vector of string that contains words
  */
-void inverse(std::vector<std::string>& dict);
-void sortAsc(const std::vector<std::string>& dict);
+void inverse(std::vector<std::string> &dict);
+
+void sortAsc(std::vector<std::string> &dict);
 
 #endif //LABO09_READ_H
